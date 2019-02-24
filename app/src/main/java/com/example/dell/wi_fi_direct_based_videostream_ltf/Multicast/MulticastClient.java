2d0c2fb238f43a9599282811e79ac921d23ad1b8
venohum <myfuture_ltf@126.com>
2019-02-24 14:14:22 +0800

    realize the real-time video transfer for performance testing

--- app/src/main/java/com/example/dell/wi_fi_direct_based_videostream_ltf/Multicast/MulticastClient.java
+++ app/src/main/java/com/example/dell/wi_fi_direct_based_videostream_ltf/Multicast/MulticastClient.java
@@ -27,6 +27,9 @@ public class MulticastClient {
             Log.d(TAG, "MulticastClient: 我想看看这是谁的IP"+getIP());
 //            multicastSocket.setNetworkInterface(NetworkInterface.getByInetAddress(InetAddress.getByName(getIP())));
             multicastSocket.joinGroup(new InetSocketAddress(InetAddress.getByName("224.0.0.1"),port),NetworkInterface.getByName(getIP()));
+
+
+
         }catch (Exception e){
             e.printStackTrace();
         }
